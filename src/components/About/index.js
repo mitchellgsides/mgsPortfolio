d93f534f9148ac7f298e3706f04a1b8e5024b6fa
mitchellgsides <mitchellgsides@gmail.com>
2020-02-16 16:10:55 -0600

    about me picture slides

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,11 +6,15 @@ import mountain from '../../assets/icon-images/maroon_bells.jpg'
 import elevate from '../../assets/icon-images/Elevate-reduced.jpg'
 
 const AboutDetails = styled.div`
-  padding: 1rem;
-  flex: 1;
+  padding: 0.5rem;
   border: 1px solid ${colors.gunmetal};
+  height: 80vh;
   border-radius: 1rem;
+  margin: 0.5rem;
   background-color: rgba(0, 0, 0, 0.2);
+  @media (max-width: 600px) {
+    width: 320px;
+  }
 `
 
 const AboutText = styled.p`
@@ -21,11 +25,11 @@ const AboutText = styled.p`
 
 const AboutContainer = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: center;
-  flex-direction: row;
   @media (max-width: 600px) {
     flex-direction: column;
+    width: 320px;
   }
   overflow-x: auto;
   @media (max-width: 600px) {
@@ -39,6 +43,8 @@ const StyledImgIcon = styled.img`
   width: 80px;
   border-radius: 0.5rem;
   margin: 0.6rem;
+  box-shadow: ${(p) => p.selected ? '0 4px 8px 0 rgba(200, 200, 200, 0.4), 0 6px 20px 0 rgba(200, 200, 200, 0.29)' : ''};
+
   @media (max-width: 600px) {
   }
   &:hover {
@@ -48,10 +54,9 @@ const StyledImgIcon = styled.img`
 `
 
 const SelectedImage = styled.img`
-  max-width: 50vw;
-  max-height: 70vh;
+  max-width: 70vw;
+  max-height: 80vh;
   margin: 0.5rem;
-  flex: 1;
   border-radius: 1rem;
   @media (max-width: 600px) {
     max-width: 320px;
@@ -60,8 +65,16 @@ const SelectedImage = styled.img`
 const ImageIconContainer = styled.div`
   display: flex;
   flex-direction: row;
+  align-items: center;
+  justify-content: center;
   flex-wrap: wrap;
   max-width: 70vw;
+  padding: 0.25rem;
+  margin: 0.25rem;
+`
+
+const ImageContainer = styled.div`
+  margin-top: 1rem;
 `
 
 const pictures = [
@@ -82,18 +95,17 @@ export default function About (props) {
 
   return (
     <AboutContainer>
-      <div>
-        <PageTitle>About Me</PageTitle>
-        <AboutDetails>
-          <AboutText>
+      <AboutDetails>
+        <PageTitle>About</PageTitle>
+        <AboutText>
         I'm a full stack developer and student of programming languages and technology. I have experience working with fast-paced, collaborative teams
         developing production-ready features very quickly. I've been successful with Agile methodologies, in particular at my first startup, where I really enjoyed the daily challenges of a rapidly evolving and
         demanding environment.
         Prior to development, I was a professional cyclist and coach, as well as a high school
         teacher of science and physical education. While coaching elite athletes using advanced
         analytical software targeted at individualizing coaching, I discovered a passion for coding, which led me to a career in software development.
-          </AboutText>
-          <AboutText>
+        </AboutText>
+        <AboutText>
           Before falling in love with the challenge of problem solving with code, I learned that a dedication to consistency and building
         on strong foundations with the help of skilled mentors leads to success, and I've carried these lessons forward
         into my pursuit of new skills and technical knowledge. When I'm not coding, I still enjoy training and
@@ -101,24 +113,20 @@ export default function About (props) {
         value spending time outdoors with my wife, Lydia, and our labrador retriever, Solomon. We
         particularly enjoy backpacking, camping, and hiking in the Rocky Mountains and across
         Texas.
-          </AboutText>
-        </AboutDetails>
-      </div>
-
-      <div>
-
-        <ImageIconContainer>
-          {pictures.map((pic, index) =>
-            <StyledImgIcon onClick={() => setSelectedPic(selectedPic && selectedPic.title === pic.title ? null : pic)} key={`${pic.title}-${index}-icon`} src={pic.source} alt={pic.alt} />
-          )}
-        </ImageIconContainer>
-        {selectedPic ? (
-          <div>
-            <h5>{selectedPic.title}</h5>
-            <SelectedImage src={selectedPic.source} alt={selectedPic.source} />
-          </div>)
-          : null}
-      </div>
+        </AboutText>
+      </AboutDetails>
+      <ImageIconContainer>
+        {pictures.map((pic, index) =>
+          <StyledImgIcon selected={selectedPic && selectedPic.title === pic.title} onClick={() => setSelectedPic(selectedPic && selectedPic.title === pic.title ? null : pic)} key={`${pic.title}-${index}-icon`} src={pic.source} alt={pic.alt} />
+        )}
+      </ImageIconContainer>
+      {selectedPic ? (
+        <ImageContainer>
+          <h5>{selectedPic.title}</h5>
+          <SelectedImage src={selectedPic.source} alt={selectedPic.source} />
+        </ImageContainer>
+      )
+        : null}
     </AboutContainer>
   )
 }
