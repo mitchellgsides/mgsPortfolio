93b7689d33f74121bc8e6447558634bea4e8d5c2
mitchellgsides <mitchellgsides@gmail.com>
2020-02-25 14:20:02 -0600

    Updates

---
+++ precache-manifest.2b59df73fe62fa9d91f6f841f0deb3f4.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "927fe1e45bd333572cdf",
+    "url": "/mgsPortfolio/static/js/main.927fe1e4.chunk.js"
+  },
+  {
+    "revision": "b4eac266cc31d427d19f",
+    "url": "/mgsPortfolio/static/js/1.b4eac266.chunk.js"
+  },
+  {
+    "revision": "8148a8a3eb1815fafad9",
+    "url": "/mgsPortfolio/static/js/runtime~main.8148a8a3.js"
+  },
+  {
+    "revision": "a70a7e3e545f3ddc5707d4c4da8cfa17",
+    "url": "/mgsPortfolio/static/media/css3-icon.a70a7e3e.svg"
+  },
+  {
+    "revision": "de79c8f14c2b0d692ae215d7e6eccd54",
+    "url": "/mgsPortfolio/static/media/javascript-icon.de79c8f1.svg"
+  },
+  {
+    "revision": "36a90fcbfe725cf973ccdc497ce8c5e2",
+    "url": "/mgsPortfolio/static/media/jquery-icon.36a90fcb.svg"
+  },
+  {
+    "revision": "c02b7da93fea7f472b89583e23f12759",
+    "url": "/mgsPortfolio/static/media/git-original.c02b7da9.svg"
+  },
+  {
+    "revision": "e9f3d01c242e086f895bde131803b451",
+    "url": "/mgsPortfolio/static/media/react-original.e9f3d01c.svg"
+  },
+  {
+    "revision": "e83e60b310396562d703c9ccff616b91",
+    "url": "/mgsPortfolio/static/media/npm-original-wordmark.e83e60b3.svg"
+  },
+  {
+    "revision": "b7dc3b99382465652bda0e61515c0937",
+    "url": "/mgsPortfolio/static/media/redux-original.b7dc3b99.svg"
+  },
+  {
+    "revision": "02867dbe5f804e4dbd0db076bb271cc4",
+    "url": "/mgsPortfolio/static/media/typescript-original.02867dbe.svg"
+  },
+  {
+    "revision": "1e83f7103d15e32357b2dc768106fac0",
+    "url": "/mgsPortfolio/static/media/postgresql-original.1e83f710.svg"
+  },
+  {
+    "revision": "ad84e47a56e2960733ade87cb6b703a7",
+    "url": "/mgsPortfolio/static/media/github-icon.ad84e47a.svg"
+  },
+  {
+    "revision": "f87066895815a4ac16dc755ccc667f5b",
+    "url": "/mgsPortfolio/static/media/maroon_bells.f8706689.jpg"
+  },
+  {
+    "revision": "31bcd7710dc1bbf61dca292e986815a0",
+    "url": "/mgsPortfolio/static/media/Elevate-reduced.31bcd771.jpg"
+  },
+  {
+    "revision": "44e7f39b24aed73d1d5345433c834255",
+    "url": "/mgsPortfolio/static/media/Tetons.44e7f39b.jpg"
+  },
+  {
+    "revision": "6d20f5094221863f49d9297091b914e4",
+    "url": "/mgsPortfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/mgsPortfolio/precache-manifest.2529c2fd299a4617c2498a9afb7d92f8.js"
+  "/mgsPortfolio/precache-manifest.2b59df73fe62fa9d91f6f841f0deb3f4.js"
 );
 
 workbox.clientsClaim();
